ff60e7191e7ba28702874e1e36100656adda3832
zhaosiwen <caoyasong1949@163.com>
2017-04-17 19:32:08 +0800

    增加了筛选视频标题的步骤。

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var title = $("#video_title>h1").text;
+var tmp = $("#video_title>h1").clone();
+tmp.find(":nth-child(n)").remove();
+var ttile = tmp.text();
 var src = $("video#myVideo").attr("src");
 
 console.log(title);
